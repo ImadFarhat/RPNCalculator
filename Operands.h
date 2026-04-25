3bb35fdbc9f4ca4137ec39d03725057e0e393f2f
Imad Farhat <imadnasserfarhat@gmail.com>
2026-04-25 12:38:18 -0500

    Added operands header file
    Default member functions initialized

---
+++ Operands.h
@@ -0,0 +1,46 @@
+//Name: Imad Farhat
+//Homework 12: RPN calculator
+#include <iostream>
+#include <stack>
+#include <queue>
+#include <deque>
+//preprocessor directives
+
+class Operand {
+public: 
+	virtual ~Operand();
+	//virtual destructor
+};
+
+class Number: public Operand {
+public:
+	Number(double num);
+	//constructor 
+	double Num;
+	double getValue();
+	//public double and function to get value
+
+};
+
+class Operator : public Operand {
+	//empty
+};
+
+class Add : public Operator {
+	//empty
+};
+
+class Subtract : public Operator {
+	//empty
+};
+
+class Mulitply : public Operator {
+	//empty
+};
+
+class Divide : public Operator {
+	//empty
+};
+class Square : public Operator {
+	//empty
+};
