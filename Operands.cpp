bdc5234600e1a3b87063d335531f9ed00e88a908
Imad Farhat <imadnasserfarhat@gmail.com>
2026-04-25 12:43:43 -0500

    Added implementation file
    Implemented default member functions and debugged corresponding logic

---
+++ Operands.cpp
@@ -0,0 +1,21 @@
+//Name: Imad Farhat
+//Homework 12: RPN calculator
+#include "Operands.h"
+
+Operand::~Operand()
+{
+	//empty
+}
+
+Number::Number(double num)
+	:Num(num)
+	//initalizes public double member variable
+{
+	//empty
+}
+
+double Number::getValue()
+{
+	return Num; 
+	//returns public double member var
+}
